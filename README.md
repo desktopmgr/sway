f1c666f0e0228a4a6adc6717284b975ebbfe04af
Jason <git-admin@casjaysdev.com>
2021-11-25 23:25:11 -0500

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ README.md
@@ -0,0 +1,47 @@
+## sway  
+  
+i3-compatible Wayland compositor  
+  
+Automatic install/update:
+
+```shell
+bash -c "$(curl -LSs https://github.com/desktopmgr/sway/raw/main/install.sh)"
+```
+
+Manual install:
+  
+requires:
+
+Debian based:
+
+```shell
+apt install sway
+```  
+
+Fedora Based:
+
+```shell
+yum install sway
+```  
+
+Arch Based:
+
+```shell
+pacman -S sway
+```  
+
+MacOS:  
+
+```shell
+brew install
+```
+  
+```shell
+mv -fv "$HOME/.config/sway" "$HOME/.config/sway.bak"
+git clone https://github.com/desktopmgr/sway "$HOME/.config/sway"
+```
+  
+<p align=center>
+  <a href="https://wiki.archlinux.org/index.php/sway" target="_blank" rel="noopener noreferrer">sway wiki</a>  |  
+  <a href="https://github.com/swaywm" target="_blank" rel="noopener noreferrer">sway site</a>
+</p>  
---
+++ etc/polybar/launch.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env sh
+
+# More info : https://github.com/jaagr/polybar/wiki
+
+# Install the following applications for polybar and icons in polybar if you are on ArcoLinuxD
+# awesome-terminal-fonts
+# Tip : There are other interesting fonts that provide icons like nerd-fonts-complete
+
+MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+desktop=$(echo $DESKTOP_SESSION)
+count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+
+
+case $desktop in
+
+    i3)
+    if type "xrandr" > /dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-i3 -c ~/.config/awesome/polybar/config &
+      done
+    else
+    polybar --reload mainbar-i3 -c ~/.config/awesome/polybar/config &
+    fi
+    # second polybar at bottom
+    # if type "xrandr" > /dev/null; then
+    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #     MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/awesome/polybar/config &
+    #   done
+    # else
+    # polybar --reload mainbar-i3-extra -c ~/.config/awesome/polybar/config &
+    # fi
+    ;;
+
+    openbox)
+    if type "xrandr" > /dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-openbox -c ~/.config/openbox/polybar/config &
+      done
+    else
+    polybar --reload mainbar-openbox -c ~/.config/openbox/polybar/config &
+    fi
+    # second polybar at bottom
+    # if type "xrandr" > /dev/null; then
+    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #     MONITOR=$m polybar --reload mainbar-openbox-extra -c ~/.config/openbox/polybar/config &
+    #   done
+    # else
+    # polybar --reload mainbar-openbox-extra -c ~/.config/openbox/polybar/config &
+    # fi
+    ;;
+
+    bspwm)
+    if type "xrandr" > /dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/bspwm/polybar/config &
+      done
+    else
+    polybar --reload mainbar-bspwm -c ~/.config/bspwm/polybar/config &
+    fi
+    # second polybar at bottom
+    # if type "xrandr" > /dev/null; then
+    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #     MONITOR=$m polybar --reload mainbar-bspwm-extra -c ~/.config/bspwm/polybar/config &
+    #   done
+    # else
+    # polybar --reload mainbar-bspwm-extra -c ~/.config/bspwm/polybar/config &
+    # fi
+    ;;
+
+    xmonad)
+    if [ $count = 1 ]; then
+      m=$(xrandr --query | grep " connected" | cut -d" " -f1)
+      MONITOR=$m polybar --reload mainbar-xmonad -c ~/.config/xmonad/polybar/config &
+    else
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-xmonad -c ~/.config/xmonad/polybar/config &
+      done
+    fi
+    # second polybar at bottom
+    # if [ $count = 1 ]; then
+    #   m=$(xrandr --query | grep " connected" | cut -d" " -f1)
+    #   MONITOR=$m polybar --reload mainbar-xmonad-extra -c ~/.config/xmonad/polybar/config &
+    # else
+    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #     MONITOR=$m polybar --reload mainbar-xmonad-extra -c ~/.config/xmonad/polybar/config &
+    #   done
+    # fi
+    ;;
+esac
